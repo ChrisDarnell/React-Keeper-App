a94b97669650bba849e28e423ae1530aab7e44fa
Chris <chrisdarnell@gmail.com>
2021-06-22 02:23:47 -0500

    React App with Material UI
    Add/Delete Notes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import NoteAddIcon from '@material-ui/icons/NoteAdd';
 
 function Header() {
-  return (
-    <header>
-      <h1><NoteAddIcon/>Keeper React App</h1>
-      <h3>by Chris Darnell</h3>
-    </header>
+  return ( <
+    header >
+    <
+    h1 > < NoteAddIcon / > Keeper React App < /h1> <
+    h3 > by Chris Darnell < /h3> <
+    /header>
   );
 }
 
