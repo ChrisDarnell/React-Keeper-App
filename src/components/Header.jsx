f259bbba04457d887ac0dd201864164de093a84e
Chris <chrisdarnell@gmail.com>
2021-06-22 02:21:16 -0500

    Styling Material UI Complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import NoteAddIcon from '@material-ui/icons/NoteAdd';
 
 function Header() {
   return (
     <header>
-      <h1>Keeper React App</h1>
+      <h1><NoteAddIcon/>Keeper React App</h1>
       <h3>by Chris Darnell</h3>
     </header>
   );
